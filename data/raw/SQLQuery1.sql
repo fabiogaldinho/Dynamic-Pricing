5ba6ea43c7a2111d9dca710c9e1f30f8892aae13
Fábio Galdino <fabio.galdino989@gmail.com>
2025-05-24 11:43:08 -0300

    Updating datasets and adding new ones

--- data/raw/SQLQuery1.sql
+++ data/raw/SQLQuery1.sql
@@ -1,4 +1,4 @@
-SELECT *
+SELECT Qtde as quantity_in, DataInc as date_in
 FROM EntradaIte
-WHERE Materia = 40
-ORDER BY Datainc DESC;
+WHERE Materia = 40 AND Datainc >= '2024-01-01'
+ORDER BY Datainc ASC;
